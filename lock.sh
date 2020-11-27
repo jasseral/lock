cca8b354ed91fe55575ac1532c4e35f99a007572
jasser <jasser.garcia@gmail.com>
2020-11-27 13:26:34 -0500

    added new version

--- lock.sh
+++ lock.sh
@@ -1,335 +1,198 @@
 #!/bin/bash
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install ubuntu-gnome-desktop
-echo "set shared/default-x-display-manager lightdm | debconf-comunicate"
+# DEFAULT USER FOLDER
+
+cat > ~/.config/autostart/firefox.desktop <<EOF
+[Desktop Entry]
+Type=Application
+Exec=firefox -private -p sandbox
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name[en_US]=firefox
+Name=firefox
+Comment[en_US]=
+Comment=
+EOF
+
+
+# Make the file  ~/dconf-settings.ini
+cat > ~/dconf-settings.ini <<EOF
+[org/gnome/settings-daemon/plugins/xsettings]
+overrides={'Gtk/ShellShowsAppMenu': <1>}
+hinting='slight'
+
+[org/gnome/shell]
+enabled-extensions=['dash-to-panel@jderose9.github.com']
+enable-hot-corners=false
+
+[org/gnome/shell/extensions/dash-to-panel]
+panel-element-positions='{"0":[{"element":"showAppsButton","visible":false,"position":"stackedTL"},{"element":"activitiesButton","visible":false,"position":"stackedTL"},{"element":"leftBox","visible":true,"position":"stackedTL"},{"element":"taskbar","visible":false,"position":"stackedTL"},{"element":"centerBox","visible":false,"position":"stackedBR"},{"element":"rightBox","visible":false,"position":"stackedBR"},{"element":"dateMenu","visible":false,"position":"stackedBR"},{"element":"systemMenu","visible":true,"position":"stackedBR"},{"element":"desktopButton","visible":false,"position":"stackedBR"}]}'
+show-apps-icon-file=''
+show-running-apps=false
+show-tooltip=false
+animate-window-launch=false
+dot-position='BOTTOM'
+panel-position='TOP'
+panel-positions='{"0":"TOP"}'
+secondarymenu-contains-appmenu=false
+dot-style-focused='METRO'
+location-clock='STATUSRIGHT'
+show-showdesktop-button=false
+animate-app-switch=false
+hot-keys=false
+dot-style-unfocused='METRO'
+hotkeys-overlay-combo='TEMPORARILY'
+show-favorites=false
+taskbar-position='CENTEREDMONITOR'
+show-show-apps-button=false
+available-monitors=[0]
+show-window-previews=false
+
+[org/gnome/gedit/preferences/ui]
+show-tabs-mode='auto'
+
+[org/gnome/gedit/plugins]
+active-plugins=['time', 'docinfo', 'modelines', 'spell', 'filebrowser']
+
+[org/gnome/gedit/state/window]
+size=(900, 700)
+state=87168
+bottom-panel-size=140
+side-panel-size=200
+side-panel-active-page='GeditWindowDocumentsPanel'
+
+[org/gnome/nautilus/preferences]
+search-filter-time-type='last_modified'
+
+[org/gnome/nautilus/desktop]
+volumes-visible=false
+trash-icon-visible=false
+
+[org/gnome/evolution-data-server]
+network-monitor-gio-name=''
+migrated=true
+
+[org/gnome/deja-dup]
+backend='goa'
+prompt-check='2020-11-27T15:25:43.072519Z'
+
+[org/gnome/deja-dup/goa]
+type='google'
+
+[org/gnome/deja-dup/file]
+migrated=true
+
+[org/gnome/desktop/app-folders]
+folder-children=['Utilities', 'Sundry', 'YaST']
+
+[org/gnome/desktop/app-folders/folders/Utilities]
+translate=true
+categories=['X-GNOME-Utilities']
+apps=['org.gnome.baobab.desktop', 'deja-dup-preferences.desktop', 'eog.desktop', 'evince.desktop', 'org.gnome.FileRoller.desktop', 'gnome-calculator.desktop', 'gnome-dictionary.desktop', 'org.gnome.Characters.desktop', 'org.gnome.DiskUtility.desktop', 'org.gnome.font-viewer.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Screenshot.desktop', 'gnome-system-log.desktop', 'gnome-system-monitor.desktop', 'gnome-tweak-tool.desktop', 'gucharmap.desktop', 'seahorse.desktop', 'vinagre.desktop', 'yelp.desktop']
+name='X-GNOME-Utilities.directory'
+
+[org/gnome/desktop/app-folders/folders/Sundry]
+translate=true
+categories=['X-GNOME-Sundry']
+apps=['alacarte.desktop', 'authconfig.desktop', 'ca.desrt.dconf-editor.desktop', 'fedora-release-notes.desktop', 'firewall-config.desktop', 'flash-player-properties.desktop', 'gconf-editor.desktop', 'gnome-abrt.desktop', 'gnome-power-statistics.desktop', 'ibus-setup-anthy.desktop', 'ibus-setup.desktop', 'ibus-setup-hangul.desktop', 'ibus-setup-libbopomofo.desktop', 'ibus-setup-libpinyin.desktop', 'ibus-setup-m17n.desktop', 'ibus-setup-typing-booster.desktop', 'im-chooser.desktop', 'itweb-settings.desktop', 'jhbuild.desktop', 'javaws.desktop', 'java-1.7.0-openjdk-jconsole.desktop', 'java-1.7.0-openjdk-policytool.desktop', 'log4j-chainsaw.desktop', 'log4j-logfactor5.desktop', 'nm-connection-editor.desktop', 'orca.desktop', 'setroubleshoot.desktop', 'system-config-date.desktop', 'system-config-firewall.desktop', 'system-config-keyboard.desktop', 'system-config-language.desktop', 'system-config-printer.desktop', 'system-config-users.desktop', 'vino-preferences.desktop']
+name='X-GNOME-Sundry.directory'
+
+[org/gnome/desktop/app-folders/folders/YaST]
+translate=true
+categories=['X-SuSE-YaST']
+name='suse-yast.directory'
+
+[org/gnome/desktop/notifications]
+application-children=['org-gnome-dejadup', 'update-manager']
+
+[org/gnome/desktop/notifications/application/org-gnome-tweaks]
+application-id='org.gnome.tweaks.desktop'
+
+[org/gnome/desktop/notifications/application/update-manager]
+application-id='update-manager.desktop'
+
+[org/gnome/desktop/notifications/application/org-gnome-dejadup]
+application-id='org.gnome.DejaDup.desktop'
+
+[org/gnome/desktop/wm/preferences]
+button-layout=':appmenu,minimize,maximize,close'
+
+[org/gnome/desktop/background]
+show-desktop-icons=true
+
+[org/gnome/desktop/calendar]
+show-weekdate=false
+
+[org/gnome/desktop/input-sources]
+sources=[('xkb', 'latam')]
+xkb-options=@as []
+
+[org/gnome/desktop/interface]
+clock-show-date=false
+gtk-im-module='gtk-im-context-simple'
+clock-show-seconds=false
+enable-animations=true
+show-battery-percentage=false
+
+[org/gnome/control-center]
+last-panel='wifi'
+
+[org/gnome/terminal/legacy]
+schema-version=uint32 3
+
+[org/gnome/software]
+check-timestamp=int64 1606490603
+
+[org/gtk/settings/file-chooser]
+sort-column='name'
+sidebar-width=196
+window-position=(358, 0)
+show-size-column=true
+show-hidden=false
+window-size=(1203, 881)
+sort-directories-first=true
+date-format='regular'
+sort-order='ascending'
+location-mode='path-bar'
+
+[apps/update-manager]
+first-run=false
+launch-time=int64 1606494347
+launch-count=2
+
+[com/ubuntu/update-notifier]
+release-check-time=uint32 1606490559
+
+EOF
+
 # DEPENDENCIES PART 
-#sudo apt-get install lightdm -y   # I NEED TEST THIS PART IF IS NECESSARY OR NOT 
-sudo apt-get install openbox-gnome-session -y  # I NEED TEST THIS PART IF IS NECESSARY OR NOT 
-sudo apt-get install openbox -y 
-sudo apt-get install obmenu -y 
-sudo apt-get install tint2 -y 
-sudo apt-get install nitrogen -y
-sudo apt-get install xdotool -y
-sudo apt install gnome-system-tools -y
+sudo apt-get update
+sudo apt-get install gnome-tweak-tool
+sudo apt-get install -y gnome-shell-extension-dash-to-panel
 
-mkdir /wallpapers
-chmod -Rf 777 /wallpapers
+# PRE LOAD CONFIGURATION
+cat ~/dconf-settings.ini | dconf load /
 
+# REMOVE ITEMS FROM CONFIGURATION-PANEL
 
-mkdir ~/.config/nitrogen/
+cd /usr/share/applications/
 
-# NITROGEN
-# Make the file  ~/.config/nitrogen/bg-saved.cfg
-cat > ~/.config/nitrogen/bg-saved.cfg <<EOF
-[xin_-1]
-file=/wallpapers/wallpaper1.png
-mode=4
-bgcolor=#000000
-
-EOF
-
-# Make the file  ~/.config/nitrogen/nitrogen.cfg
-cat > ~/.config/nitrogen/nitrogen.cfg <<EOF
-[geometry]
-posx=0
-posy=0
-sizex=510
-sizey=500
-
-[nitrogen]
-view=list
-recurse=true
-sort=alpha
-icon_caps=false
-dirs=/wallpapers;
-
-EOF
-
-
-# OPENBOX
-mkdir ~/.config/openbox/
-# Make the file  ~/.config/openbox/autostart.sh
-cat > ~/.config/openbox/autostart.sh <<EOF
-tint2 &
-nitrogen --restore &
-#firefox -private -p sandbox & yoursite search --sync --onlyvisible --pid $! windowactivate key F11
-
-EOF
-
-# GET WALLPAPERS 
-# Go to Download folder
-cd /wallpapers
-wget https://raw.githubusercontent.com/jasseral/lock/master/wallpaper1.png
-wget https://raw.githubusercontent.com/jasseral/lock/master/wallpaper2.png
-
-
-
-# Make the file ~/.config/openbox/menu.xml
-cat >  ~/.config/openbox/menu.xml  <<EOF
-
-<?xml version="1.0" encoding="utf-8"?>
-<openbox_menu xmlns="http://openbox.org/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://openbox.org/               file:///usr/share/openbox/menu.xsd">
-    <menu id="root-menu" label="Openbox 3">
-     <item label="Terminal emulator">
-       <action name="Execute">
-         <execute>x-terminal-emulator</execute>
-       </action>
-     </item>
-     <item label="Edit autostart">
-       <action name="Execute">
-         <execute>gedit ~/.config/openbox/autostart.sh</execute>
-       </action>
-     </item>
-     <separator/>
-     <!-- This requires the presence of the 'openbox-menu' package to work -->
-     <item label="Web browser">
-       <action name="Execute">
-         <execute>firefox -private -p sandbox & yoursite search --sync --onlyvisible --pid  windowactivate key F11</execute>
-       </action>
-     </item>
-     <separator/>
-     <item label="Change wallpaper">
-       <action name="Execute">
-         <execute>nitrogen</execute>
-       </action>
-     </item>
-     <separator/>
-     <item label="Restart">
-       <action name="Execute">
-         <execute>reboot</execute>
-       </action>
-     </item>
-     <separator/>
-     <item label="Shutdown">
-       <action name="Execute">
-         <execute>shutdown now</execute>
-       </action>
-     </item>
-    </menu>
-</openbox_menu>
-
-
-EOF
-
-# END_OPENBOX_COFIGURATION
-
-#TINT2
-mkdir ~/.config/tint2/
-# Make the file  userContent.css
-cat > ~/.config/tint2/tint2rc <<EOF
-
-#---- Generated by tint2conf ba0a ----
-# See https://gitlab.com/o9000/tint2/wikis/Configure for
-# full documentation of the configuration options.
-#-------------------------------------
-# Gradients
-#-------------------------------------
-# Backgrounds
-# Background 1: Panel
-rounded = 0
-border_width = 0
-border_sides = TBLR
-border_content_tint_weight = 0
-background_content_tint_weight = 0
-background_color = #000000 60
-border_color = #000000 30
-background_color_hover = #000000 60
-border_color_hover = #000000 30
-background_color_pressed = #000000 60
-border_color_pressed = #000000 30
-
-# Background 2: Default task, Iconified task
-rounded = 4
-border_width = 1
-border_sides = TBLR
-border_content_tint_weight = 0
-background_content_tint_weight = 0
-background_color = #777777 20
-border_color = #777777 30
-background_color_hover = #aaaaaa 22
-border_color_hover = #eaeaea 44
-background_color_pressed = #555555 4
-border_color_pressed = #eaeaea 44
-
-# Background 3: Active task
-rounded = 4
-border_width = 1
-border_sides = TBLR
-border_content_tint_weight = 0
-background_content_tint_weight = 0
-background_color = #777777 20
-border_color = #ffffff 40
-background_color_hover = #aaaaaa 22
-border_color_hover = #eaeaea 44
-background_color_pressed = #555555 4
-border_color_pressed = #eaeaea 44
-
-# Background 4: Urgent task
-rounded = 4
-border_width = 1
-border_sides = TBLR
-border_content_tint_weight = 0
-background_content_tint_weight = 0
-background_color = #aa4400 100
-border_color = #aa7733 100
-background_color_hover = #cc7700 100
-border_color_hover = #aa7733 100
-background_color_pressed = #555555 4
-border_color_pressed = #aa7733 100
-
-# Background 5: Tooltip
-rounded = 1
-border_width = 1
-border_sides = TBLR
-border_content_tint_weight = 0
-background_content_tint_weight = 0
-background_color = #222222 100
-border_color = #333333 100
-background_color_hover = #ffffaa 100
-border_color_hover = #000000 100
-background_color_pressed = #ffffaa 100
-border_color_pressed = #000000 100
-
-#-------------------------------------
-# Panel
-panel_items = S
-panel_size = 100% 30
-panel_margin = 0 0
-panel_padding = 2 0 2
-panel_background_id = 1
-wm_menu = 1
-panel_dock = 0
-panel_position = bottom center horizontal
-panel_layer = top
-panel_monitor = all
-panel_shrink = 0
-autohide = 1
-autohide_show_timeout = 0
-autohide_hide_timeout = 0.5
-autohide_height = 2
-strut_policy = follow_size
-panel_window_name = tint2
-disable_transparency = 1
-mouse_effects = 1
-font_shadow = 0
-mouse_hover_icon_asb = 100 0 10
-mouse_pressed_icon_asb = 100 0 0
-scale_relative_to_dpi = 0
-scale_relative_to_screen_height = 0
-
-#-------------------------------------
-# Taskbar
-taskbar_mode = single_desktop
-taskbar_hide_if_empty = 0
-taskbar_padding = 0 0 2
-taskbar_background_id = 0
-taskbar_active_background_id = 0
-taskbar_name = 1
-taskbar_hide_inactive_tasks = 0
-taskbar_hide_different_monitor = 0
-taskbar_hide_different_desktop = 0
-taskbar_always_show_all_desktop_tasks = 0
-taskbar_name_padding = 4 2
-taskbar_name_background_id = 0
-taskbar_name_active_background_id = 0
-taskbar_name_font_color = #e3e3e3 100
-taskbar_name_active_font_color = #ffffff 100
-taskbar_distribute_size = 0
-taskbar_sort_order = none
-task_align = left
-
-#-------------------------------------
-# Task
-task_text = 1
-task_icon = 1
-task_centered = 1
-urgent_nb_of_blink = 100000
-task_maximum_size = 150 35
-task_padding = 2 2 4
-task_tooltip = 1
-task_thumbnail = 0
-task_thumbnail_size = 210
-task_font_color = #ffffff 100
-task_background_id = 2
-task_active_background_id = 3
-task_urgent_background_id = 4
-task_iconified_background_id = 2
-mouse_left = toggle_iconify
-mouse_middle = none
-mouse_right = close
-mouse_scroll_up = toggle
-mouse_scroll_down = iconify
-
-#-------------------------------------
-# System tray (notification area)
-systray_padding = 0 4 2
-systray_background_id = 0
-systray_sort = ascending
-systray_icon_size = 24
-systray_icon_asb = 100 0 0
-systray_monitor = 1
-systray_name_filter =
-
-#-------------------------------------
-# Launcher
-launcher_padding = 2 4 2
-launcher_background_id = 0
-launcher_icon_background_id = 0
-launcher_icon_size = 24
-launcher_icon_asb = 100 0 0
-launcher_icon_theme_override = 0
-startup_notifications = 1
-launcher_tooltip = 1
-launcher_item_app = /usr/share/applications/nm-connection-editor.desktop
-
-#-------------------------------------
-# Clock
-time1_format = %H:%M
-time2_format = %A %d %B
-time1_timezone =
-time2_timezone =
-clock_font_color = #ffffff 100
-clock_padding = 2 0
-clock_background_id = 0
-clock_tooltip =
-clock_tooltip_timezone =
-clock_lclick_command =
-clock_rclick_command = orage
-clock_mclick_command =
-clock_uwheel_command =
-clock_dwheel_command =
-
-#-------------------------------------
-# Battery
-battery_tooltip = 1
-battery_low_status = 10
-battery_low_cmd = notify-send "battery low"
-battery_full_cmd =
-battery_font_color = #ffffff 100
-bat1_format =
-bat2_format =
-battery_padding = 1 0
-battery_background_id = 0
-battery_hide = 101
-battery_lclick_command =
-battery_rclick_command =
-battery_mclick_command =
-battery_uwheel_command =
-battery_dwheel_command =
-ac_connected_cmd =
-ac_disconnected_cmd =
-
-#-------------------------------------
-# Tooltip
-tooltip_show_timeout = 0.5
-tooltip_hide_timeout = 0.1
-tooltip_padding = 4 4
-tooltip_background_id = 5
-tooltip_font_color = #dddddd 100
-
-
-EOF
-
+sudo rm gnome-bluetooth-panel.desktop
+sudo rm gnome-background-panel.desktop
+sudo rm gnome-notifications-panel.desktop
+sudo rm gnome-search-panel.desktop
+sudo rm gnome-region-panel.desktop
+sudo rm gnome-universal-access-panel.desktop
+sudo rm gnome-online-accounts-panel.desktop
+sudo rm gnome-privacy-panel.desktop
+sudo rm gnome-sharing-panel.desktop
+sudo rm gnome-sound-panel.desktop
+sudo rm gnome-power-panel.desktop
+sudo rm gnome-network-panel.desktop
+sudo rm gnome-removable-media-panel.desktop
+sudo rm gnome-info-overview-panel.desktop
+sudo rm rhythmbox-device.desktop
 
 # ALL THIS PART IS FOR FIREFOX-CUSTOMIZATION
 
@@ -407,7 +270,7 @@ display: none;
 
 #PanelUI-menu-button
 {
-	display: none !important;
+  display: none !important;
 }
 
 #TabsToolbar { visibility: collapse !important; }
@@ -435,17 +298,17 @@ EOF
 # Make the file  userContent.css
 cat > ~/.mozilla/firefox/$PROFPATH/chrome/userContent.css <<EOF
 @-moz-document url(about:blank),
-           	url(about:preferences),
-           	url(about:config),
-           	url(about:newtab)
+            url(about:preferences),
+            url(about:config),
+            url(about:newtab)
 {
   #newtab-window,
   html,
   body,
   #newtab-customize-overlay
   {
-	color: #b2b2b2 !important;
-	display: none !important;
+  color: #b2b2b2 !important;
+  display: none !important;
 
   }
 }
@@ -453,6 +316,5 @@ EOF
 
 
 # END FIREFOX-CUSTOMIZATION
-
-# END_TINT_COFIGURATION
 sudo reboot   
+
